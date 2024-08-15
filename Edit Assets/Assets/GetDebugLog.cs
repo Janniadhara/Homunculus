266cdd91e39302fdccffc7a6e8b39f78c4f8f279
Janniadhara <janni.adhara@gmail.com>
2024-08-16 00:50:38 +0200

    logwarnings apear on screen

---
+++ Edit Assets/Assets/GetDebugLog.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using static UnityEditor.Rendering.CameraUI;
+
+public class GetDebugLog : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI debugLogTextField;
+    void OnEnable()
+    {
+        Application.logMessageReceived += HandleLog;
+    }
+
+    void OnDisable()
+    {
+        Application.logMessageReceived -= HandleLog;
+    }
+
+    void HandleLog(string logString, string stackTrace, LogType type)
+    {
+        if (type == LogType.Error || type == LogType.Exception || type == LogType.Assert)
+        {
+            debugLogTextField.text = logString;
+        }
+    }
+}
